0060479d9e5395d7a51cf8164dae62fdaa9f272e
Yukari Chiba <i@0x7f.cc>
2025-07-31 16:15:54 +0800

    add ufetch support for k1 and eic7700

--- utils/uboot/clone.sh
+++ utils/uboot/clone.sh
@@ -4,6 +4,8 @@ export GIT_CLONE_PROTECTION_ACTIVE=false
 
 mkdir -p work
 
+if [ ! -d work/uboot ]; then
+
 git clone --depth=1 -b "${UBOOT_BRANCH:-master}" "${UBOOT_GIT:-https://github.com/u-boot/u-boot.git}" work/uboot
 
 if [ ! -z $UBOOT_SUBDIR ]; then
@@ -13,4 +15,6 @@ fi
 
 if [ ! -z $UBOOT_SBI ]; then
 	git clone --depth=1 -b "${UBOOT_SBI_BRANCH:-master}" "${UBOOT_SBI_GIT:-https://github.com/riscv/opensbi.git}" work/sbi
+fi
+
 fi
--- utils/uboot/patch.sh
+++ utils/uboot/patch.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+if [ ${#UBOOT_PATCHES[@]} -ne 0 ]; then
+        for patchfile in "${UBOOT_PATCHES[@]}"; do
+                if [ -f ../../uboot/patches/$patchfile ]; then
+	                patch -p1 < ../../uboot/patches/$patchfile
+		fi
+        done
+fi
