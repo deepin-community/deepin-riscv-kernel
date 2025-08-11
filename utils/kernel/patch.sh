2b7714b113ebe12b09f41a9aedf8ee09d84c42dd
Yukari Chiba <i@0x7f.cc>
2025-08-11 10:59:47 +0800

    add remote patch support

--- utils/kernel/patch.sh
+++ utils/kernel/patch.sh
@@ -4,6 +4,13 @@ if [ ${#KERNEL_PATCHES[@]} -ne 0 ]; then
         for patchfile in "${KERNEL_PATCHES[@]}"; do
                 if [ -f ../../kernel/patches/$patchfile ]; then
 	                patch -p1 < ../../kernel/patches/$patchfile
+		elif [[ $patchfile == http://* ]] || [[ $patchfile == https://* ]]; then
+			patchfile_tmp=$(mktemp)
+			wget $patchfile -O $patchfile_tmp
+			patch -p1 < $patchfile_tmp
+		else
+			echo "error: patch $patchfile not found"
+			exit 1
 		fi
         done
 fi
