dca791b3d3adf7788d24b0a48cfcebbbc880ca78
Yukari Chiba <i@0x7f.cc>
2025-09-02 19:43:51 +0800

    kernel: disable kernel headers when cross compiling

--- utils/kernel/compile.sh
+++ utils/kernel/compile.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ "$GCC_ARCH" != "x86_64" ]; then
+  export DEB_BUILD_PROFILES="pkg.linux-upstream.nokernelheaders"
+else
+  export KERNEL_ENABLE_MODULES=1
+fi
+
 $MAKE_EXEC bindeb-pkg -j$(nproc) LOCALVERSION="-${TARGET_PROFILE}"
