0d0717e07134efbffc940b49c35ff8cb41ae917f
Yukari Chiba <i@0x7f.cc>
2025-11-07 16:42:16 +0800

    uboot: do not clean after build

--- utils/uboot/compile.sh
+++ utils/uboot/compile.sh
@@ -31,12 +31,7 @@ mkdir -p $OUTPUT_DIR/${UBOOT_OUTPUT_DIR}
 
 for UBOOT_RESULT_FILENAME in "${UBOOT_RESULT_FILENAMES[@]}"; do
 	find . -name "$UBOOT_RESULT_FILENAME" | xargs -I @ cp -av @ $OUTPUT_DIR/${UBOOT_OUTPUT_DIR}/
+	find . -name "$UBOOT_RESULT_FILENAME" -delete
 done
 
-$MAKE_EXEC clean
-
-done
-
-for MISC_RESULT_FILENAME in "${MISC_RESULT_FILENAMES[@]}"; do
-	find . -name "$MISC_RESULT_FILENAME" | xargs -I @ cp -av @ $OUTPUT_DIR/
 done
