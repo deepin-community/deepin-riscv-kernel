e24c8f3c7c66eb8a4ed39e9c56416d5bf3753ec8
Yukari Chiba <i@0x7f.cc>
2026-09-10 13:49:55 +0800

    all: support multiple toolchain

--- utils/toolchain.sh
+++ utils/toolchain.sh
@@ -31,31 +31,35 @@ extract_tarball() {
 	esac
 }
 
-if [ -n "$TOOLCHAIN" ]; then
-	if [ -f "$TOOLCHAIN" ]; then
-		echo "Extracting toolchain from $TOOLCHAIN ..."
-		toolchain_dir="$PWD/work/toolchain"
-		rm -rf "$toolchain_dir"
-		mkdir -p "$toolchain_dir"
-		extract_tarball "$TOOLCHAIN" "$toolchain_dir"
-		export PATH="$toolchain_dir/bin:$PATH"
-		echo "Toolchain extracted to $toolchain_dir, PATH updated"
-	elif [ -d "$TOOLCHAIN" ]; then
-		echo "Using toolchain directory $TOOLCHAIN ..."
-		export PATH="$TOOLCHAIN/bin:$PATH"
-		echo "PATH updated with $TOOLCHAIN/bin"
-	else
-		toolchain_url="$TOOLCHAIN"
-		toolchain_dir="$PWD/work/toolchain"
-		toolchain_archive="$PWD/work/toolchain.tar"
-		echo "Downloading toolchain from $toolchain_url ..."
-		rm -rf "$toolchain_dir" "$toolchain_archive"*
-		mkdir -p "$toolchain_dir"
-		wget -q "$toolchain_url" -O "$toolchain_archive"
-		echo "Extracting toolchain ..."
-		extract_tarball "$toolchain_archive" "$toolchain_dir"
-		rm -f "$toolchain_archive"
-		export PATH="$toolchain_dir/bin:$PATH"
-		echo "Toolchain extracted to $toolchain_dir, PATH updated"
-	fi
+if [ ${#TOOLCHAIN[@]} -gt 0 ]; then
+	idx=0
+	for tc in "${TOOLCHAIN[@]}"; do
+		if [ -f "$tc" ]; then
+			echo "Extracting toolchain from $tc ..."
+			toolchain_dir="$PWD/work/toolchain-$idx"
+			rm -rf "$toolchain_dir"
+			mkdir -p "$toolchain_dir"
+			extract_tarball "$tc" "$toolchain_dir"
+			export PATH="$toolchain_dir/bin:$PATH"
+			echo "Toolchain extracted to $toolchain_dir, PATH updated"
+		elif [ -d "$tc" ]; then
+			echo "Using toolchain directory $tc ..."
+			export PATH="$tc/bin:$PATH"
+			echo "PATH updated with $tc/bin"
+		else
+			toolchain_url="$tc"
+			toolchain_dir="$PWD/work/toolchain-$idx"
+			toolchain_archive="$PWD/work/toolchain-$idx.tar"
+			echo "Downloading toolchain from $toolchain_url ..."
+			rm -rf "$toolchain_dir" "$toolchain_archive"*
+			mkdir -p "$toolchain_dir"
+			wget -q "$toolchain_url" -O "$toolchain_archive"
+			echo "Extracting toolchain ..."
+			extract_tarball "$toolchain_archive" "$toolchain_dir"
+			rm -f "$toolchain_archive"
+			export PATH="$toolchain_dir/bin:$PATH"
+			echo "Toolchain extracted to $toolchain_dir, PATH updated"
+		fi
+		idx=$((idx + 1))
+	done
 fi
